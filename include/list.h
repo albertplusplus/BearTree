682a40bf9f3a8aaee019740f8c916a604ec2d590
albertplusplus <albertbear94@gmail.com>
2025-03-11 16:43:52 -0500

    Added min and max to linked list

--- include/list.h
+++ include/list.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 
+
 namespace bear {
 
     template <typename T>
@@ -19,6 +20,8 @@ namespace bear {
 
         size_t _size;
         std::shared_ptr<Node> head, tail;
+        T _min;
+        T _max;
 
     public:
     list() : head{nullptr}, tail{nullptr}, _size{0} {}
@@ -28,22 +31,29 @@ namespace bear {
             if (empty()) {
                 head = std::make_shared<Node>(_item);
                 tail = head;
+                _min = _max =  _item;
+                ++_size;
                 return;
             }
             auto curr = std::make_shared<Node>(_item);
             curr->next = head;
             head->prev = curr;
             head = curr;
+            if (head->item < _min) _min = head->item;
+            if (head->item > _max) _max = head->item;
+            ++_size;
         }
 
         void push_back(T _item) {
             if (empty()) {
                 push_front(_item);
+                return;
             }
             auto curr = std::make_shared<Node>(_item);
             curr->prev = tail;
             tail->next = curr;
             tail = curr;
+            ++_size;
         }
 
         T peek_front() {
@@ -59,7 +69,6 @@ namespace bear {
         T pop_front() {
             if (!head) throw std::runtime_error("Tried popping empty list");
             auto ret = head->item;
-
             if (!(head->next)) {
                 head = nullptr;
                 tail = nullptr;
@@ -67,6 +76,9 @@ namespace bear {
                 head = head->next;
                 head->prev = nullptr;
             }
+            if (ret == _min) update_min();
+            if (ret == _max) update_max();
+            _size -= 1;
             return ret;
         }
 
@@ -81,6 +93,9 @@ namespace bear {
                 tail = tail->prev;
                 tail->next = nullptr;
             }
+            if (ret == _min) update_min();
+            if (ret == _max) update_max();
+            _size -= 1;
             return ret;
         }
 
@@ -96,7 +111,39 @@ namespace bear {
             }
         }
 
+        T min() {
+            if (empty()) return std::runtime_error("List is empty");
+            return _min;
+        }
+
+        T max() {
+            if (empty()) return std::runtime_error("List is empty");
+            return _max;
+        }
+
         size_t size() { return _size; }
+
+    private:
+
+        T update_min() {
+            auto curr = head.get();
+            while (curr != nullptr) {
+                if (curr->item < _min) {
+                    _min = curr->item;
+                }
+                curr = curr->next.get();
+            }
+        }
+
+        T update_max() {
+            auto curr = head.get();
+            while (curr != nullptr) {
+                if (curr->item > _max) {
+                    _max = curr->item;
+                }
+                curr = curr->next.get();
+            }
+        }
     };
 }
 
